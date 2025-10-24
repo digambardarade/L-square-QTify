9bad446b0b602e5cd3877fb55b7add0084d96520
digambardarade <digambardarade@gmail.com>
2025-10-24 22:55:38 +0530

    Logo png issu resolved

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./Logo.module.css";
-import LogoImg from "../../assets/logo.png";
+//import LogoImg from "../../assets/logo.png";
 
 const Logo=()=>{
 
 return(
     <div className={styles.logoDiv}>
-        <img src={LogoImg} width={67} alt="logo"/>
+        <img src={require("../../assets/logo.png")} width={67} alt="logo"/>
     </div>
 )
 };
